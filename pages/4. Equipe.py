3b42a5aa68f5dae59ab5f7e5eeb5177031e6b4f6
SARAHGURGEL <sarahgurgel@gmail.com>
2022-11-24 17:32:19 -0300

    ok

--- pages/4. Equipe.py
+++ pages/4. Equipe.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown(''' # EQUIPE
-    ### Este trabalho foi realizado no âmbito do Curso Bootcamp em Ciência de Dados - Batch 997 - Enap/Le Wagon por:
+    ### Este trabalho foi realizado no âmbito do Curso Bootcamp em Ciência de Dados - Batch 997 - Enap/Le Wagon, em novembro/2022, por:
     #### Fernanda Teixeira Reis
     #### Jairo Luiz Silveira Filho
     #### Paulo Roberto De Sousa Freitas Filho
