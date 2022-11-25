35322009359972cb7cc2a9706112372168281872
SARAHGURGEL <sarahgurgel@gmail.com>
2022-11-25 11:43:48 -0300

    ok

--- pages/2. Metodologia_de_categorização.py
+++ pages/2. Metodologia_de_categorização.py
@@ -14,7 +14,11 @@ st.markdown('''
     ##### ▪ Streamlit
     #\n
     ### Fonte de dados:
-    ##### ▪ IEX = VTE/VTD.
+    # ''')
+
+st.latex(r'''▪  IEx = \left(\frac{VTE}{VTD}\right)''')
+
+st.markdown('''
     ##### ▪ VTE (Valor Total Executado): Sistema de Gestão Financeira do Fundo Nacional de Desenvolvimento da Educação (SIGEF/PDDE/FNDE/MEC).
     ##### ▪ VTD (Valor Total Repassado Durante o Período ): Sistema de Ações Educacionais do PDDE do Fundo Nacional de Desenvolvimento da Educação (SAE/PDDE/FNDE/MEC).
     ##### ▪ Dados de estrutura escolar: Censo Escolar da Educação Básica (INEP/MEC). Disponíveis em: http://inep.gov.br/web/guest/resultados-e-resumos..
